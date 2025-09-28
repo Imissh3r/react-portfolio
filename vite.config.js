4120f6fccb30b0d06981c2f6fc12943f1aa78e62
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-09-28 18:28:23 +0000

    feat: enable PWA support for portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Sree Dev Portfolio',
+        short_name: 'SreePortfolio',
+        description: 'My personal React developer portfolio',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64',
+            type: 'image/x-icon'
+          },
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
